62f6c791f8cb783ef0c266344238ad2f5943d596
Sylvain Hallé <shalle@acm.org>
2025-10-08 06:18:44 -0400

    Tweaks

--- Source/config.lua
+++ Source/config.lua
@@ -1,1 +1,9 @@
 -- Paramètres du formulaire
+
+-- Prénom et nom du stagiaire
+PrenomStagiaire = "Marty"
+NomStagiaire    = "McFly"
+
+-- Prénom et nom du superviseur
+PrenomSuperviseur = "Emmett"
+NomSuperviseur    = "Brown"
